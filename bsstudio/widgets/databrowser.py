14c43de0b1a2fe9e590e72e149c66aedca4ff853
bsobhani <bsobhani@bnl.gov>
2020-06-30 13:16:09 -0400

    making databrowser table instead of list

--- bsstudio/widgets/databrowser.py
+++ bsstudio/widgets/databrowser.py
@@ -3,8 +3,11 @@ from . import CodeContainer
 from .REButton import makeProperty
 from PyQt5.QtCore import QDateTime
 from PyQt5.QtWidgets import QDateTimeEdit
-from PyQt5.QtWidgets import QListWidget, QFrame, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QAbstractItemView
+from PyQt5.QtWidgets import QListWidget, QTableWidget, QTableWidgetItem, QFrame, QVBoxLayout, QLabel, QPushButton
 from bsstudio.functions import widgetValue, plotHeader
+from collections.abc import Iterable
+
 
 class DataBrowser(CodeContainer):
 	def __init__(self, parent):
@@ -15,7 +18,8 @@ class DataBrowser(CodeContainer):
 		self._plotArgsList = "[[]]"
 		self._plotKwargsList = "[{}]"
 		layout = QVBoxLayout()
-		self.listWidget = QListWidget()
+		#self.listWidget = QListWidget()
+		self.listWidget = QTableWidget()
 		now = QDateTime.currentDateTime()
 		self.startDateTime = QDateTimeEdit(now.addMonths(-6))
 		self.endDateTime = QDateTimeEdit(now)
@@ -32,7 +36,8 @@ class DataBrowser(CodeContainer):
 		#self.startDateTime.dateTimeChanged.connect(self.__updateTable)
 		#self.endDateTime.dateTimeChanged.connect(self.__updateTable)
 		self.loadScansButton.clicked.connect(self.__updateTable)
-		self.listWidget.currentTextChanged.connect(self.__replot)
+		#self.listWidget.currentTextChanged.connect(self.__replot)
+		self.listWidget.currentItemChanged.connect(self.__replot)
 
 	def __updateTable(self):
 		self.runCode()
@@ -44,16 +49,42 @@ class DataBrowser(CodeContainer):
 		self._replot()
 
 	def updateTable(self, db, dbKwargs):
-		self.listWidget.clear()
+		#self.listWidget.clear()
 		since = self.startDateTime.dateTime().toString("yyyy-MM-dd HH:mm:ss")
 		until = self.endDateTime.dateTime().toString("yyyy-MM-dd HH:mm:ss")
-		results = db(since=since, until=until, **dbKwargs)
+		results = list(db(since=since, until=until, **dbKwargs))
+		self.listWidget.setRowCount(len(results))
+		self.listWidget.setSortingEnabled(True)
+		self.listWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+		col_set=set([])
 		for r in results:
-			self.listWidget.addItem(r.start['uid'])
+			cols = [k for k,v in r.start.items() if not isinstance(v,Iterable) or type(v)==str]
+			col_set.update(cols)	
+		cols = list(col_set)
+		self.listWidget.setColumnCount(len(cols))
+		self.listWidget.setHorizontalHeaderLabels(cols)
+		for i in range(len(results)):
+			r = results[i]
+			#self.listWidget.addItem(r.start['uid'])
+			for j in range(len(cols)):
+				#item = QTableWidgetItem(r.start['uid'])
+				print(cols[j])
+				print(r.start[cols[j]])
+				item = QTableWidgetItem(str(r.start[cols[j]]))
+				self.listWidget.setItem(i,j,item)
+
+	def findHorizontalHeaderIndex(self, key):
+		for i in range(self.listWidget.columnCount()):
+			if self.listWidget.horizontalHeaderItem(i).text()==key:
+				return i
+		return None
 
 	def replot(self, plots, db):
 		from copy import copy
-		item = self.listWidget.currentItem()
+		#item = self.listWidget.currentItem()
+		row = self.listWidget.currentRow()
+		uid_col = self.findHorizontalHeaderIndex("uid")
+		item = self.listWidget.item(row,uid_col)
 		print("item",item)
 		if item is None:
 			return
