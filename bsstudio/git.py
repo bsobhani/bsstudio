9e71135223502de9339f61786710ccc784b2499b
bsobhani <bsobhani@bnl.gov>
2020-08-03 10:44:36 -0400

    adding git.py

---
+++ bsstudio/git.py
@@ -0,0 +1,57 @@
+import cola
+import cola.app
+import cola.main
+from cola.widgets.browse import worktree_browser
+from cola.widgets.commitmsg import CommitMessageEditor
+from cola.widgets.status import StatusWidget
+from PyQt5.QtWidgets import QApplication
+
+cola.app.initialize()
+args = cola.main.parse_args(["cola"])
+context = cola.app.application_init(args)
+
+def cmd_browse(args):
+	from cola.widgets.browse import worktree_browser  # pylint: disable=all
+
+	context = cola.app.application_init(args)
+	view = worktree_browser(context, show=False, update=False, settings=args.settings)
+	#return cola.app.application_run(context, view)
+	cola.app.initialize_view(context, view)
+	cola.app.default_start(context, view)
+	#return context.app.start()
+	return view
+
+def run():
+
+	#view = worktree_browser(context,show=False)
+	#view.show()
+	#return view
+	
+	#return cola.main.cmd_browse(args)
+	return cmd_browse(args)
+
+def make_commit():
+	commit_editor = CommitMessageEditor(context, None)
+	commit_editor.show()
+	return commit_editor
+
+def make_status():
+	class TB:
+		def add_corner_widget(self, val):
+			pass
+	titlebar = TB()
+	status_editor = StatusWidget(context, titlebar, None)
+	status_editor.show()
+	return status_editor
+	
+	
+
+if __name__=="__main__":
+	app1 = QApplication([])
+	#commit_editor = make_commit()
+	#status_editor = make_status()
+	#print(context.model)
+	view = run()
+	view.show()
+	app1.exec_()
+	print("here")
